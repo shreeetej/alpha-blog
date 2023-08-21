e2e50983685acbe1b386b3f1e2cef7f50f32e3c4
Shreetej <shritej.sonkamble3@outlook.com>
2023-08-21 10:21:48 +0530

    Added CRUD actions for editing and displaying flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   get 'contact', to: 'pages#contact'
 
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 
 end
