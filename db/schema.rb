8cf8ab55b1c0c1a361399d97d2a287e9338e922f
Shreetej <shritej.sonkamble3@outlook.com>
2023-08-24 11:55:36 +0530

    Added users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_084754) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_061623) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_084754) do
     t.datetime "updated_at", null: false
     t.text "URL"
     t.string "created_by"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
