1ada0cd4779a8ab41496cfae378013bb7448725c
Shreetej <shritej.sonkamble3@outlook.com>
2023-09-08 10:47:08 +0530

    Added edit users functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+  end
+
   def new
     @user = User.new
   end
@@ -14,8 +19,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user = User.update(user_params) && @user.password?
+      redirect_to articles_path
+      flash[:notice] = "Updated your profile successfully"
+    else
+      render 'edit'
+    end
+  end
+
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
